1e8bcaf06ce5388f8f978427f93e904fc236e50a
sjs <seo777111@gmail.com>
2022-06-14 23:19:02 +0900

    vue event

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,6 +1,21 @@
+//el: e돔에서  어떤 샐러터에 마운팅 될지
+
 new Vue({
   el: '#app',
   data: {
-    msg: 'hello world'
+    query: 'sss'
+  },
+  methods: {
+    onSubmit(e) {
+      debugger
+    },
+    onReset(){
+      this.query = ''
+      // todo  검색 결과 숨기기
+      debugger
+    },
+    onKeyup(){
+      //if(!this.query.length) this.onReset()
+    }
   }
 })
